7a57b00c1d53e6ce44244e3cfc13137c3dd04493
Lucas Vinicius Sillis do Vale <vale@alunos.utfpr.edu.br>
2025-06-06 17:48:02 -0300

    test: insert banch test for step motor in raspberr

---
+++ motor_test.py
@@ -0,0 +1,8 @@
+from dispenser_controller.dispenser import MedicineDispenser
+
+step_pin = 17
+dir_pin = 27
+relay_pin = 22
+dispenser = MedicineDispenser(step_pin, dir_pin, relay_pin)
+dispenser.run([2,9,14])
+
